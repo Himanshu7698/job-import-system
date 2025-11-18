63a3d2040de43e8fa85c402f3e2eb15edf92c298
Himanshu7698 <himanshugajera97@gmail.com>
2025-11-19 00:50:51 +0530

    error-solve

--- client/app/socket.ts
+++ client/app/socket.ts
@@ -1,28 +1,28 @@
-import { io } from 'socket.io-client';
+// import { io } from 'socket.io-client';
 
-const socket = io(`${process.env.NEXT_APP_SOCKET_URL}`, {
-  autoConnect: false,
-  withCredentials: true,
-  path: process.env.VITE_APP_SOCKET_PATH,
-  auth: {
-    token: getToken(),
-  },
-});
+// const socket = io(`${process.env.NEXT_APP_SOCKET_URL}`, {
+//   autoConnect: false,
+//   withCredentials: true,
+//   path: process.env.VITE_APP_SOCKET_PATH,
+//   auth: {
+//     token: getToken(),
+//   },
+// });
 
-export const connectSocket = () => {
-  socket.connect();
+// export const connectSocket = () => {
+//   socket.connect();
 
-  socket.on('connect', () => {
-    console.log('✅ Socket connected!', socket.id);
-    // After connect, tell server to send messages
-    socket.emit('Initialize');
-  });
+//   socket.on('connect', () => {
+//     console.log('✅ Socket connected!', socket.id);
+//     // After connect, tell server to send messages
+//     socket.emit('Initialize');
+//   });
 
-  socket.on('disconnect', (reason) => console.log('❌ Socket disconnected:', reason));
+//   socket.on('disconnect', (reason) => console.log('❌ Socket disconnected:', reason));
 
-  socket.on('connect_error', (err) => console.error('❌ Socket connection error:', err.message));
+//   socket.on('connect_error', (err) => console.error('❌ Socket connection error:', err.message));
 
-  socket.on('UnAuthorized', (data) => console.warn('🚫 Unauthorized socket:', data));
-};
+//   socket.on('UnAuthorized', (data) => console.warn('🚫 Unauthorized socket:', data));
+// };
 
-export default socket;
+// export default socket;
